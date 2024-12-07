1bd3144c6c449e1303d6cd4f05d19d0251817123
metepg <mete.guneysel@gmail.com>
2024-12-08 00:40:47 +0200

    Change sealed classes to enums

--- src/main/kotlin/com/metepg/JumpMode.kt
+++ src/main/kotlin/com/metepg/JumpMode.kt
@@ -1,12 +1,9 @@
 package com.metepg
 
-sealed class JumpMode(private val value: Int) {
-    object CHAR1: JumpMode(0)
-    object CHAR2: JumpMode(1)
-    object WORD0: JumpMode(2)
-    object WORD1: JumpMode(3)
-    object LINE: JumpMode(4)
-
-    override fun toString() = value.toString()
-
+enum class JumpMode {
+    CHAR1,
+    CHAR2,
+    WORD0,
+    WORD1,
+    LINE;
 }
--- src/main/kotlin/com/metepg/finder/InputState.kt
+++ src/main/kotlin/com/metepg/finder/InputState.kt
@@ -1,10 +1,7 @@
 package com.metepg.finder
 
-sealed class InputState(private val value: Int) {
-    object WAIT_SEARCH_CHAR1: InputState(0)
-    object WAIT_SEARCH_CHAR2: InputState(1)
-    object WAIT_KEY: InputState(2)
-
-    override fun toString() = value.toString()
-
+enum class InputState {
+    WAIT_SEARCH_CHAR1,
+    WAIT_SEARCH_CHAR2,
+    WAIT_KEY;
 }
