838a871eba51a69e1b211c1b296f6f2b88678fca
metepg <mete.guneysel@gmail.com>
2024-12-08 19:38:05 +0200

    Version 2.0.1
    - Opt out of kotlin standard library. This reduces the build size from ~1.6MB to ~55Kb

--- src/main/kotlin/com/metepg/finder/Char1Finder.kt
+++ src/main/kotlin/com/metepg/finder/Char1Finder.kt
@@ -5,21 +5,22 @@ import com.metepg.MarksCanvas
 import com.metepg.utils.getMarksFromAllEditors
 
 class Char1Finder: Finder {
-    private var state = InputState.ADD_CHAR1_TO_SEARCH
+    private lateinit var state: InputState
 
     override fun start(e: Editor): List<MarksCanvas.Mark>? {
+        state = InputState.AddChar1ToSearch
         return null
     }
 
     override fun input(e: Editor, c: Char, lastMarks: List<MarksCanvas.Mark>): List<MarksCanvas.Mark> = when (state) {
-        InputState.ADD_CHAR1_TO_SEARCH -> addChar1ToSearch(e, c)
-        InputState.SHOW_MARKS -> advanceMarks(c, lastMarks)
+        InputState.AddChar1ToSearch -> addChar1ToSearch(e, c)
+        InputState.ShowMarks -> advanceMarks(c, lastMarks)
         else -> throw IllegalStateException("Unexpected state: $state")
     }
 
     private fun addChar1ToSearch(e: Editor, c: Char): List<MarksCanvas.Mark> {
         val pattern = Regex(Regex.escape(c.toString()), RegexOption.IGNORE_CASE)
-        state = InputState.SHOW_MARKS
+        state = InputState.ShowMarks
         return e.project.getMarksFromAllEditors(pattern)
     }
 }
--- src/main/kotlin/com/metepg/finder/InputState.kt
+++ src/main/kotlin/com/metepg/finder/InputState.kt
@@ -1,7 +1,9 @@
 package com.metepg.finder
 
-enum class InputState {
-    ADD_CHAR1_TO_SEARCH,
-    ADD_CHAR2_TO_SEARCH,
-    SHOW_MARKS;
+sealed class InputState(private val value: Int) {
+    object AddChar1ToSearch: InputState(1)
+    object AddChar2ToSearch: InputState(1)
+    object ShowMarks: InputState(2)
+
+    override fun toString() = value.toString()
 }
